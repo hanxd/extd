c11679972fb0a8cd3779cac24ff52119f91ff985
hxd <X.HanXD@Gmail.com>
2021-02-05 12:22:45 +0800

    Delete ext-rooter-basic directory

--- ext-rooter-basic/files/usr/lib/lua/luci/controller/guestwifi.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.guestwifi", package.seeall)
-
-function index()
-	local page
-
-	page = entry({"admin", "network", "guestwifi"}, cbi("guestwifi", {hidesavebtn=true, hideresetbtn=true}), "Guest Wifi", 22)
-	page.dependent = true
-end
